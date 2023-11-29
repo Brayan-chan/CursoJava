6601f22110b247adcd21625f00706e9845a77862
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-11-29 01:14:06 -0600

    Update EjemploDos.java

--- EjemploDos/src/ejemplodos/EjemploDos.java
+++ EjemploDos/src/ejemplodos/EjemploDos.java
@@ -1,20 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejemplodos;
 
-/**
- *
- * @author chanp
- */
 public class EjemploDos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Palabras reservadas en java");
+        /*Son pañabras, que solo el lenguaje de programación
+        Java, puede utilizar como identificadores, porque 
+        tienen una función especial.
+        */
+        
+        /*Por tal motivo, no puede ser utilizadas por
+        el programador, como identificadores para
+        sus programas
+        */
+        
+        //EJEMPLO
+        //CORRECTO             int valor_uno = 0;
+        //INCORRECTO           int for = 0;
+        
+        System.out.println("ACTUALMENTE EXISTEN 50 PALABRAS RESERVADAS EN JAVA");
+        /*
+        abstract        case        continue
+        assert          catch       default
+        boolean         char        do
+        break           class       double
+        byte            const       else
+        */
+        
+        /*
+        extends         goto                int
+        final           if                  interface
+        finally         implements          long
+        float           import              native
+        for             instanceof          new
+        */
+        
+        /*
+        package         short               synchronized
+        private         static              this   
+        protected       strictfp            throw
+        public          super               throws
+        return          switch              trannsient
+        */
+        
+        /*
+        try
+        void
+        volatile
+        while
+        enum
+        */
     }
     
 }
