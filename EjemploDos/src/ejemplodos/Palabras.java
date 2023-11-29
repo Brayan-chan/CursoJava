2e1df5eff6d96dd745dc9325090c088a5f347ddf
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-11-29 01:14:31 -0600

    Update Palabras.java

--- EjemploDos/src/ejemplodos/Palabras.java
+++ EjemploDos/src/ejemplodos/Palabras.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejemplodos;
 
-/**
- *
- * @author chanp
- */
 public class Palabras {
+    int valor_uno = 0;
     
+    //INCORRECTO
+    // int for = 0;
+    
+    int _for = 0;
 }
